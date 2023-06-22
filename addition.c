50d19112bf35bf18621fc88a072a5d63f11d83da
Siddhu <cpushpa354@gmail.com>
2023-06-22 13:09:21 +0000

    Commited the index.html

--- addition.c
+++ addition.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c;
-	a=10,b=20,c;
-	c=a+b;
-	printf("The addition of two numbers is:"c);
+	int a,b,c,d;
+	a=10,b=20,c=40,d;
+	d=a+b+c;
+	printf("The addition of three numbers is:"d);
 }
